3eb0edd612228e36ff9f0503c2759c969a6ab2d4
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-04-28 13:16:13 -0400

    changes coat check

--- SMS-Weather-Bot/smsWeatherBot.py
+++ SMS-Weather-Bot/smsWeatherBot.py
@@ -19,16 +19,16 @@ account_sid = keys.twilioSID
 auth_token = keys.twilioAuthToken
 client = Client(account_sid, auth_token)
 
-coatcheckToday = 'so wear a jacket!' if weatherBot.chanceOfRain > 20 else '.'
-coatcheckTmrw = 'so wear a jacket tomorrow!' if weatherBot.tmrwChanceOfRain > 20 else '.'
+coatcheckToday = '- so wear a jacket!' if weatherBot.chanceOfRain > 20 else '.'
+coatcheckTmrw = '-so wear a jacket tomorrow!' if weatherBot.tmrwChanceOfRain > 20 else '.'
 
 weatherMessage = f'\nCurrent temp: {weatherBot.currentTempF:.0f}\u00b0F but it feels like {weatherBot.currentFeelsLikeTempF:.0f}\u00b0F' \
                 f'\nToday\'s high: {weatherBot.maxTempF:.0f}\u00b0F' \
                 f'\nToday\'s low: {weatherBot.minTempF:.0f}\u00b0F' \
-                f'\nToday\'s chance of rain: {weatherBot.chanceOfRain}% - {coatcheckToday}' \
+                f'\nToday\'s chance of rain: {weatherBot.chanceOfRain}% {coatcheckToday}' \
                 f'\n\nTomorrow\'s High: {weatherBot.tmrwMaxTempF:.0f}\u00b0F' \
                 f'\nTomorrow\'s Low: {weatherBot.tmrwMinTempF:.0f}\u00b0F' \
-                f'\nChance of rain tomorrow: {weatherBot.tmrwChanceOfRain}% - {coatcheckTmrw} '\
+                f'\nChance of rain tomorrow: {weatherBot.tmrwChanceOfRain}% {coatcheckTmrw} '\
 
 message = client.messages \
 				.create(
